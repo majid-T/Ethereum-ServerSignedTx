b0da1d5ae30597a0a49ef4795b3fc09865a591ff
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-06-16 04:38:53 +0430

    Added json abi and web3 objects.. all working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async function (req, res, next) {
+  // making a Web3 object
+  const Web3 = require("web3");
+
+  //making a transaction object
+  var Tx = require("ethereumjs-tx").Transaction;
+
+  //connecting to an ethereum network --- here is ganache GUI
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider("http://localhost:7545")
+  );
+
+  //Getting the account on the network
+  var accounts = await web3.eth.getAccounts();
+  console.log("Accounts:", accounts);
+
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
